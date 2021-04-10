9a3560f4872fe0523f10a68a2378fd5091fe5692
Amy <amyseo@ucla.edu>
2021-04-10 14:25:47 -0700

    Profile page with keynote speaker done

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 import ProfileCard from "../components/ProfileCard.js";
+import AndrewSelbst from "../assets/andrew-selbst.jpg";
 
 const Container = styled("div")`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled("div")`
+    padding-top: 5vh;
+    padding-bottom: 3vh;
+    font-family: 'Westwood Sans';
+    font-size: 48px;
+    display: flex;
+    align-items: flex-start;
+`;
+
+const AllContainer = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
 export default class Profiles extends React.Component {
     render() {
         return (
-            <div>
-                Profiles
+            <AllContainer>
+                <Title>Profiles</Title>
                 <Container>
-                    <ProfileCard name="First Last" description="blurb blurb blurb" pic="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F09%2F22%2F50-cute-dog-names.jpg"></ProfileCard>
-                    <ProfileCard name="First Last" description="blurb blurb blurb" pic="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F09%2F22%2F50-cute-dog-names.jpg"></ProfileCard>
-                    <ProfileCard name="First Last" description="blurb blurb blurb" pic="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F09%2F22%2F50-cute-dog-names.jpg"></ProfileCard>
+                    <ProfileCard name="Andrew Selbst" description="Andrew Selbst is an Assistant Professor of Law at UCLA School of Law. Prior to joining UCLA Law, he was a Postdoctoral Scholar at the Data & Society Research Institute and a Visiting Fellow at Yale Law School’s Information Society Project. He has also previously taught as an Adjunct Professor at Fordham Law School.
+Selbst’s research examines the relationship between law, technology, and society. Drawing on resources from computer science, critical theory, sociology, and science, technology and society, he seeks to understand how the creation, use, and proliferation of different technologies can interfere with existing legal regimes, and how legal actors can most usefully anticipate or respond to the social effects of new technology. Over the last several years, Selbst’s research has focused on the effects of machine learning and artificial intelligence on varied legal regimes, including discrimination, policing, credit regulation, data protection, and tort law." pic={AndrewSelbst}></ProfileCard>
                 </Container>
-            </div>
+                </AllContainer>
         )
     }
 }
