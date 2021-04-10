48191ee4db5563092776cc0ee75bf21c58bb92d7
Amy <amyseo@ucla.edu>
2021-04-10 14:28:44 -0700

    resources page

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -2,29 +2,61 @@ import React from "react";
 import styled from "styled-components";
 
 const ResourceContainer = styled("div")`
-display: flex;
-flex-direction: row;
-padding-top: 3vh;
-padding-bottom: 3vh;
+    display: flex;
+    flex-direction: column;
+    padding-top: 3vh;
+    padding-bottom: 3vh;
+`;
+
+const Title = styled("div")`
+    padding-top: 5vh;
+    padding-bottom: 3vh;
+    font-family: 'Westwood Sans';
+    font-size: 48px;
+    display: flex;
+    align-items: flex-start;
 `;
 
 const AllContainer = styled("div")`
-display: flex;
-flex-direction: column;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
+const ResourceButton = styled("button")`
+    background-color: #E5ECEE;
+    text-decoration: none;
+    border: none;
+    box-shadow: none;
+    border-radius: 1vh;
+    padding: 1vh 2vh;
+    text-align: center;
+    display: inline-block;
+    font-size: 18px;
+    transition: 0.25s all ease;
+    font-family: 'Open Sans', sans-serif;
+    &:hover{
+        cursor: pointer; 
+        padding: 1.25vh 2.5vh;
+        font-size: 22px;
+    }
+    margin-bottom: 4vh;
+`;
 
 export default class Resources extends React.Component {
     render() {
         return (
-            <div>
-                Resources
-                <AllContainer>
-                    <ResourceContainer>
-
-                    </ResourceContainer>
-                </AllContainer>
-            </div>
+            <AllContainer>
+                <Title>Resources</Title>
+                <ResourceContainer>
+                    <a href="google.com"><ResourceButton>Data Privacy Workshop</ResourceButton></a>
+                    <a href="google.com"><ResourceButton>Sustainability Workshop</ResourceButton></a>
+                    <a href="google.com"><ResourceButton>Example Pitch Slides</ResourceButton></a>
+                    <a href="google.com"><ResourceButton>Intro Track Slides</ResourceButton></a>
+                    <a href="google.com"><ResourceButton>Rubric</ResourceButton></a>
+                </ResourceContainer>
+            </AllContainer>
         )
     }
 }
