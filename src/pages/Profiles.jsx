f8a61075b055806882da66bd4312f178598f4a27
Nisha McNealis <nisha.mcnealis@gmail.com>
2021-04-11 18:31:32 -0700

    add dustin + other minor changes

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import ProfileCard from "../components/ProfileCard.js";
 import AndrewSelbst from "../assets/andrew-selbst.jpg";
+import DustinBrown from "../assets/dustin-brown.jpg";
 
 const Container = styled("div")`
     display: flex;
@@ -33,7 +34,16 @@ export default class Profiles extends React.Component {
                 <Title>Profiles</Title>
                 <Container>
                     <ProfileCard name="Andrew Selbst" description="Andrew Selbst is an Assistant Professor of Law at UCLA School of Law. Prior to joining UCLA Law, he was a Postdoctoral Scholar at the Data & Society Research Institute and a Visiting Fellow at Yale Law School’s Information Society Project. He has also previously taught as an Adjunct Professor at Fordham Law School.
-Selbst’s research examines the relationship between law, technology, and society. Drawing on resources from computer science, critical theory, sociology, and science, technology and society, he seeks to understand how the creation, use, and proliferation of different technologies can interfere with existing legal regimes, and how legal actors can most usefully anticipate or respond to the social effects of new technology. Over the last several years, Selbst’s research has focused on the effects of machine learning and artificial intelligence on varied legal regimes, including discrimination, policing, credit regulation, data protection, and tort law." pic={AndrewSelbst}></ProfileCard>
+                      Selbst’s research examines the relationship between law, technology, and society. Drawing on resources from computer science, critical theory, sociology, and science, technology and society, he seeks to understand how the creation, use, and proliferation of different technologies can interfere with existing legal regimes, and how legal actors can most usefully anticipate or respond to the social effects of new technology. Over the last several years, Selbst’s research has focused on the effects of machine learning and artificial intelligence on varied legal regimes, including discrimination, policing, credit regulation, data protection, and tort law." pic={AndrewSelbst}>
+                    </ProfileCard>
+                    <ProfileCard name="Dustin Brown" description="Dustin Brown is a PhD candidate in the Electrical and Computer Engineering Department at
+                      UCLA and a Teaching Fellow for the Engineering Department. As a member of the UCLA
+                      Antenna Research, Analysis, and Measurement Laboratory, he conducts academic research on
+                      the design of phased arrays and near field measurement schemes for millimeter wave systems.
+                      He has taught three undergraduate courses focusing on public policy for emerging technology,
+                      engineering ethics, writing, and team collaboration: ENGR 182EW (Technology and Society),
+                      ENGR 183EW (Engineering and Society), and ENGR 185EW (Art of Engineering Endeavors)." pic={DustinBrown}>
+                    </ProfileCard>
                 </Container>
                 </AllContainer>
         )
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -32,13 +32,13 @@ export default class Schedule extends React.Component {
             <AllContainer>
                 <Title>Schedule</Title>
                 <Container>
-                    <Event time="10:00 AM - 10:30 AM PDT" description="Opening ceremony & keynote speaker"></Event>
-                    <Event time="10:30 AM - 11:00 AM PDT" description="Asynchronous team formation in #team-formation"></Event>
-                    <Event time="11:00 AM" description="Pitching begins!"></Event>
-                    <Event time="12:00 PM - 12:30 PM PDT" description="Policy kahoot!"></Event>
-                    <Event time="1:30 PM - 2:30 PM PDT" description="Networking"></Event>
-                    <Event time="3:00 PM PDT" description="Judging"></Event>
-                    <Event time="4:00 PM PDT - 4:30 PM PDT" description="Closing presentation"></Event>
+                    <Event time="🌟 10:00 AM - 10:30 AM PDT" description="Opening ceremony & keynote speaker"></Event>
+                    <Event time="🌟 10:30 AM - 11:00 AM PDT" description="Asynchronous team formation in #team-formation"></Event>
+                    <Event time="🌟 11:00 AM" description="Pitching begins!"></Event>
+                    <Event time="🌟 12:00 PM - 12:30 PM PDT" description="Policy kahoot!"></Event>
+                    <Event time="🌟 1:30 PM - 2:30 PM PDT" description="Networking"></Event>
+                    <Event time="🌟 3:00 PM PDT" description="Judging"></Event>
+                    <Event time="🌟 4:00 PM PDT - 4:30 PM PDT" description="Closing presentation"></Event>
                 </Container>
             </AllContainer>
         )
